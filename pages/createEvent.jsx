e9460bf8f995e4194e472fa6186f9481f231c614
Evans007 <ebounce500@gmail.com>
2023-05-10 08:26:36 +0100

    Added utils, Abi and Contract addr, and create event component

---
+++ pages/createEvent.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState} from "react";
+import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
+import ABI from '../utils/ABI/factoryAbi.json';
+import { contractAddress } from '../utils/contractAddr';
+import { toast } from "react-toastify";
+
+
+const CreateEvent = () => {
+    
+}
---
+++ utils/contractAddr.jsx
@@ -0,0 +1,1 @@
+export const contractAddr = '0xBE7256f69Bdcc1493c1092e7a90D471E055B5ADd';
