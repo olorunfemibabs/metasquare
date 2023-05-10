babd1668ce80d863148f50889ecb80999c8154e4
Evans007 <ebounce500@gmail.com>
2023-05-10 14:26:45 +0100

    trying to implement NFT storage

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import "@/styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider, midnightTheme } from "@rainbow-me/rainbowkit";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+import { configureChains, createClient, sepolia, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, bscTestnet, bsc } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import Layout from "@/components/Layout";
 
 const { chains, provider } = configureChains(
-  [mainnet, polygon, optimism, arbitrum],
+  [mainnet, polygon, optimism, arbitrum, bscTestnet, bsc],
   [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
 );
 const { connectors } = getDefaultWallets({
@@ -25,7 +25,7 @@ const wagmiClient = createClient({
 export default function App({ Component, pageProps }) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains} theme={midnightTheme()}>
+      <RainbowKitProvider modalSize="compact" chains={chains} theme={midnightTheme()}>
         <Layout provider={provider}>
           <Component {...pageProps} />
         </Layout>
