1b26ccfcc5b845250ced6cfc458e5b9453c2b1cc
olorunfemibabs <olorunfemisamuel01@gmail.com>
2023-05-13 23:12:58 +0100

    pulled

--- pages/events.jsx
+++ pages/events.jsx
@@ -1,30 +1,30 @@
 import React, { useEffect, useState } from "react";
-import EventCard from "@/components/EventCard";
 import { useContractRead } from "wagmi";
 import ABI from "../utils/ABI/factoryAbi.json";
 import contractAddr from "../utils/contractAddr";
+import EventCard from "@/components/EventCad";
 
 const events = () => {
-  const [events, setEvents ] = useState([]);
+  const [events, setEvents] = useState([]);
   const { data, isLoading, isError } = useContractRead({
     address: contractAddr,
     abi: ABI,
     functionName: "showTotalEventAddresses",
-    onSuccess(data){
+    onSuccess(data) {
       setEvents(data);
-    }
+    },
   });
 
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-col">
       Events
       <div className="grid grid-cols-3 gap-[35px]">
         {events?.map((e, i) => {
           return (
             <div key={i}>
-              <EventCard eventAddress={e}/>
+              <EventCard eventAddress={e} />
             </div>
-          )
+          );
         })}
       </div>
     </div>
