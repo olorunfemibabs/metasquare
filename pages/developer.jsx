24e27bbb3ac8375544b9776b07a71c91fcd29d37
Jerydam <jerydan148@gmail.com>
2023-05-13 20:03:53 +0100

    dev page

--- pages/developer.jsx
+++ pages/developer.jsx
@@ -11,15 +11,168 @@ const Developer = () => {
               <div>
                 <img
                   className="w-full h-auto rounded-lg"
-                  src="https://via.placeholder.com/400x400"
+                  src="/stanley.jpeg"
                   alt="Developer Avatar"
                 />
+                <div className="mt-4 flex space-x-4">
+                  <a
+                    href="https://www.linkedin.com/in/stanley-dera-137025235/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center bg-white hover:text-blue-100  hover:bg-blue-700 text-[blue] font-bold py-2 px-3 rounded"
+                  >
+                    <img
+                      src="/linkedin.svg"
+                      alt="LinkedIn Icon"
+                      className="w-4 h-4 mr-2 bg-blue-800"
+                    />
+                    LinkedIn
+                  </a>
+                  <a
+                    href="https://www.twitter.com/stanley-dera/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center bg-white hover:text-blue-100  hover:bg-blue-700 text-[blue] font-bold py-2 px-3 rounded"
+                  >
+                    <img
+                      src="/twitter.png"
+                      alt="Twitter Icon"
+                      className="w-4 h-4 mr-2"
+                    />
+                    Twitter
+                  </a>
+                  <a
+                    href="https://www.Facebook.com/stanley-dera/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center bg-white hover:text-blue-100  hover:bg-blue-700 text-[blue] font-bold py-2 px-3 rounded"
+                  >
+                    <img
+                      src="/facebook.svg"
+                      alt="Facebook Icon"
+                      className="w-4 h-4 mr-2 bg-blue-800"
+                    />
+                    Facebook
+                  </a>
+                </div>
               </div>
               <div>
-                <h2 className="text-xl font-semibold mb-2">John Doe</h2>
-                <p>Email: john.doe@example.com</p>
-                <p>Location: New York, USA</p>
-                <p>Skills: JavaScript, React, Node.js</p>
+                <h2 className="text-xl font-semibold mb-2">Onwuatu Stanley Chidera</h2>
+                <p>Email: playsanchi8@gmail.com</p>
+                <p>Location: Abuja, Nigeria</p>
+                <p>Title: Blockchain Developer</p>
+              </div>
+            </div>
+             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
+              <div>
+                <img
+                  className="w-full h-auto rounded-lg"
+                  src="/jery.jpg"
+                  alt="Developer Avatar"
+                />
+                <div className="mt-4 flex space-x-4">
+                  <a
+                    href="https://www.linkedin.com/in/stanley-dera-137025235/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center bg-white hover:text-blue-100  hover:bg-blue-700 text-[blue] font-bold py-2 px-3 rounded"
+                  >
+                    <img
+                      src="/linkedin.svg"
+                      alt="LinkedIn Icon"
+                      className="w-4 h-4 mr-2 bg-blue-800"
+                    />
+                    LinkedIn
+                  </a>
+                  <a
+                    href="https://www.twitter.com/stanley-dera/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center bg-white hover:text-blue-100  hover:bg-blue-700 text-[blue] font-bold py-2 px-3 rounded"
+                  >
+                    <img
+                      src="/twitter.png"
+                      alt="Twitter Icon"
+                      className="w-4 h-4 mr-2"
+                    />
+                    Twitter
+                  </a>
+                  <a
+                    href="https://www.Facebook.com/stanley-dera/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center bg-white hover:text-blue-100  hover:bg-blue-700 text-[blue] font-bold py-2 px-3 rounded"
+                  >
+                    <img
+                      src="/facebook.svg"
+                      alt="Facebook Icon"
+                      className="w-4 h-4 mr-2 bg-blue-800"
+                    />
+                    Facebook
+                  </a>
+                </div>
+              </div>
+              <div>
+                <h2 className="text-xl font-semibold mb-2">Jeremiah Oyeniran Damilare</h2>
+                <p>Email: jerydan148@gmail.com</p>
+                <p>Location: Ibadan, Nigeria</p>
+                <p>Title: Blockchain Developer</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
+              <div>
+                <img
+                  className="w-full h-auto rounded-lg"
+                  src="/evans.jpeg"
+                  alt="Developer Avatar"
+                />
+                <div className="mt-4 flex space-x-4">
+                  <a
+                    href="https://www.linkedin.com/in/okoli-evans"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center bg-white hover:text-blue-100  hover:bg-blue-700 text-[blue] font-bold py-2 px-3 rounded"
+                  >
+                    <img
+                      src="/linkedin.svg"
+                      alt="LinkedIn Icon"
+                      className="w-4 h-4 mr-2 bg-blue-800"
+                    />
+                    LinkedIn
+                  </a>
+                  <a
+                    href="https://www.twitter.com/OkoliEvans/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center bg-white hover:text-blue-100  hover:bg-blue-700 text-[blue] font-bold py-2 px-3 rounded"
+                  >
+                    <img
+                      src="/twitter.png"
+                      alt="Twitter Icon"
+                      className="w-4 h-4 mr-2"
+                    />
+                    Twitter
+                  </a>
+                  <a
+                    href="https://www.Facebook.com/stanley-dera/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center bg-white hover:text-blue-100  hover:bg-blue-700 text-[blue] font-bold py-2 px-3 rounded"
+                  >
+                    <img
+                      src="/facebook.svg"
+                      alt="Facebook Icon"
+                      className="w-4 h-4 mr-2 bg-blue-800"
+                    />
+                    Facebook
+                  </a>
+                </div>
+              </div>
+              <div>
+                <h2 className="text-xl font-semibold mb-2">Okoli Evans</h2>
+                <p>Email: evansdegreat@gmail.com</p>
+                <p>Location: Anambara, Nigeria</p>
+                <p>Title: Blockchain Developer</p>
               </div>
             </div>
           </div>
